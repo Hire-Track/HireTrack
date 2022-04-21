6b195c5b1e5c735e0c3ba336fd5fe0f2b91da456
pkparimi <pavanparimi92@gmail.com>
2022-04-21 15:31:42 -0400

    Schema started

---
+++ server/middleware/errorMiddleware.js
@@ -0,0 +1,17 @@
+const errorHandler = (err, req, res, next) => {
+
+    // get status code of error, else 500 (server error)
+    const statusCode = res.statusCode ? res.statusCode : 500
+    
+    // set status and respond with error message
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        // show stack only if in dev mode
+        stack: process.env.NODE_ENV === 'production' ? undefined : err.stack
+    })
+}
+
+module.exports = {
+    errorHandler
+}
