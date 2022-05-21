45e210a7bceb3169cd29f097cf540c012b804631
ajss25 <alexshinjs@gmail.com>
2022-05-20 18:14:59 -0700

    Edit/save for details page

--- client/src/pages/job-dashboard-page/JobDetails.js
+++ client/src/pages/job-dashboard-page/JobDetails.js
@@ -14,6 +14,26 @@ function JobDetails() {
     setJob({ ...job, [name]: value });
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    e.persist();
+    const token = localStorage.getItem("token");
+
+    // PUT to DB
+    fetch(`/api/jobs/${job._id}`, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      },
+      body: JSON.stringify(job)
+    }).then(onSubmitSuccess()).catch(err => console.error(err))
+  }
+
+  const onSubmitSuccess = () => {
+    window.location.href = "/job-dashboard"
+  }
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -43,19 +63,19 @@ function JobDetails() {
 			if (job._id === id) {
 				 each = 
       	{ 
-					id: job._id,
-					title: job.jobTitle, 
-					company: job.jobCompany,
-					location: job.jobLocation,
-					application: job.appLink,
-					type: job.jobType,
-					description: job.jobDescription,
-					applied: job.dateApplied.slice(0, 10),
-					response: job.dateResponse.slice(0, 10),
-					interview: job.dateInterview.slice(0, 10),
-					offer: job.dateOffer.slice(0, 10),
-					status: job.appStatus,
-					next: job.nextSteps,
+					_id: job._id,
+					jobTitle: job.jobTitle, 
+					jobCompany: job.jobCompany,
+					jobLocation: job.jobLocation,
+					appLink: job.appLink,
+					jobType: job.jobType,
+					jobDescription: job.jobDescription,
+					dateApplied: (job.dateApplied === undefined || job.dateApplied === null) ? undefined : job.dateApplied.slice(0, 10),
+					dateResponse: (job.dateResponse === undefined || job.dateResponse === null) ? undefined : job.dateResponse.slice(0, 10),
+					dateInterview: (job.dateInterview === undefined || job.dateInterview === null) ? undefined : job.dateInterview.slice(0, 10),
+					dateOffer: (job.dateOffer === undefined || job.dateOffer === null) ? undefined : job.dateOffer.slice(0, 10),
+					appStatus: job.appStatus,
+					nextSteps: job.nextSteps,
 					decision: job.decision
       	}
 			}
@@ -67,62 +87,72 @@ function JobDetails() {
     <div className="add-job-padding">
 			{setLoading}
       <h3 className="jobs-header-2">Job Details</h3>
-      <Form>
+      <Form onSubmit={onSubmit}>
         <Form.Group className="form-padding">
-          <Form.Control required placeholder="Title" defaultValue={job.title} name="jobTitle"></Form.Control>
+          <Form.Control required placeholder="Title" defaultValue={job.jobTitle} onSelect={(e)=> onChange(e)} onChange={(e)=> onChange(e)} name="jobTitle">
+          </Form.Control>
         </Form.Group>
 
         <Form.Group className="form-padding">
-          <Form.Control required placeholder="Company" defaultValue={job.company} name="jobCompany"></Form.Control>
+          <Form.Control required placeholder="Company" defaultValue={job.jobCompany} onSelect={(e)=> onChange(e)} onChange={(e)=> onChange(e)} name="jobCompany">
+          </Form.Control>
         </Form.Group>
 
         <Form.Group className="form-padding">
-          <Form.Control placeholder="Location" defaultValue={job.location} name="jobLocation"></Form.Control>
+          <Form.Control placeholder="Location" defaultValue={job.jobLocation} onSelect={(e)=> onChange(e)} onChange={(e)=> onChange(e)} name="jobLocation">
+          </Form.Control>
         </Form.Group>
 
         <Form.Group className="form-padding">
-          <Form.Control placeholder="Link to Application" defaultValue={job.application} name="appLink"></Form.Control>
+          <Form.Control placeholder="Link to Application" defaultValue={job.appLink} onSelect={(e)=> onChange(e)} onChange={(e)=> onChange(e)} name="appLink">
+          </Form.Control>
         </Form.Group>
 
         <Form.Group className="form-padding">
-          <Form.Control required as="select" onChange={(e)=> onChange(e)} aria-label="Default select example" name="type">
-            <option value="" selected disabled>Type: {job.type}</option>
+          <Form.Control as="select" onChange={(e)=> onChange(e)} aria-label="Default select example" name="jobType">
+            <option value="" selected disabled>Type: {job.jobType}</option>
             <option value="INTERNSHIP">INTERNSHIP</option>
             <option value="FULLTIME">FULLTIME</option>
           </Form.Control>
         </Form.Group>
 
         <Form.Group>
-          <Form.Control placeholder="Description: skills, salary, and benefits as applicable" defaultValue={job.description} name="jobDescription"></Form.Control>
+          <Form.Control placeholder="Description: skills, salary, and benefits as applicable" 
+            defaultValue={job.jobDescription} onSelect={(e)=> onChange(e)} onChange={(e)=> onChange(e)} name="jobDescription">
+          </Form.Control>
         </Form.Group>
         <br />
 
         <div style={{ color: "#5dbb79" }}>Dates</div>
         <Form.Group>
-          <Form.Control type="date" placeholder="Applied Date" value={job.applied} name="dateApplied"></Form.Control>
+          <Form.Control type="date" placeholder="Applied Date" value={job.dateApplied} onChange={(e)=> onChange(e)} name="dateApplied">
+          </Form.Control>
         </Form.Group>
         <div className="form-label">Applied Date</div>
 
         <Form.Group>
-          <Form.Control type="date" placeholder="Response Date" value={job.response} name="dateResponse"></Form.Control>
+          <Form.Control type="date" placeholder="Response Date" value={job.dateResponse} onChange={(e)=> onChange(e)} name="dateResponse">
+          </Form.Control>
         </Form.Group>
         <div className="form-label">Response Date</div>
 
         <Form.Group>
-          <Form.Control type="date" placeholder="Interview Date" value={job.interview} name="dateInterview"></Form.Control>
+          <Form.Control type="date" placeholder="Interview Date" value={job.dateInterview} onChange={(e)=> onChange(e)} name="dateInterview">
+          </Form.Control>
         </Form.Group>
         <div className="form-label">Interview Date</div>
 
         <Form.Group>
-          <Form.Control type="date" placeholder="Offer Date" value={job.offer} name="dateOffer"></Form.Control>
+          <Form.Control type="date" placeholder="Offer Date" value={job.dateOffer} onChange={(e)=> onChange(e)} name="dateOffer">
+          </Form.Control>
         </Form.Group>
         <div className="form-label">Offer Date</div>
         <br />
 
         <div style={{ color: "#5dbb79" }}>Process</div>
         <Form.Group className="form-padding">
-          <Form.Control required as="select" onChange={(e)=> onChange(e)} aria-label="Default select example" name="status">
-            <option value="" selected disabled>Status: {job.status}</option>
+          <Form.Control as="select" onChange={(e)=> onChange(e)} aria-label="Default select example" name="appStatus">
+            <option value="" selected disabled>Status: {job.appStatus}</option>
             <option value="APPLIED">APPLIED</option>
             <option value="WAITING">WAITING</option>
             <option value="INTERVIEW SCHEDULED">INTERVIEW SCHEDULED</option>
@@ -131,12 +161,13 @@ function JobDetails() {
         </Form.Group>
 
         <Form.Group className="form-padding">
-          <Form.Control placeholder="Next steps" defaultValue={job.next} name="nextSteps"></Form.Control>
+          <Form.Control placeholder="Next steps" defaultValue={job.nextSteps} onSelect={(e)=> onChange(e)} onChange={(e)=> onChange(e)} name="nextSteps">
+          </Form.Control>
         </Form.Group>
 
-        
         <Form.Group className="form-padding">
-          <Form.Control placeholder="Decision" defaultValue={job.decision} name="decision"></Form.Control>
+          <Form.Control placeholder="Decision" defaultValue={job.decision} onSelect={(e)=> onChange(e)} onChange={(e)=> onChange(e)} name="decision">
+          </Form.Control>
         </Form.Group>
         <br />
 
@@ -154,9 +185,9 @@ function JobDetails() {
         </Form.Group>
         <br /> */}
 
-        <Button type="submit">Add</Button>
+        <Button type="submit">Save</Button>
         <Link to="/job-dashboard">
-          <Button style={{ marginLeft: "0.5rem" }}>Cancel</Button>
+          <Button style={{ marginLeft: "0.5rem" }}>Go Back</Button>
         </Link>
         <br />
         <br />
